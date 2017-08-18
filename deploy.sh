48ad5b58bbc662586bcac8457c2939c5aa5c49a5
rbaker <rick.baker@quibids.com>
2017-08-18 14:32:38 -0500

    initial checkin

--- deploy.sh
+++ deploy.sh
@@ -43,3 +43,32 @@ aws elasticbeanstalk create-application-version --application-name $NAME-applica
 # Update the environment to use the new application version
 aws elasticbeanstalk update-environment --environment-name $NAME \
       --version-label $VERSION
+
+deploystart=$(date +%s)
+timeout=3000 # Seconds to wait before error. If it's taking awhile - your boxes probably are too small.
+threshhold=$((deploystart + timeout))
+while true; do
+    # Check for timeout
+    timenow=$(date +%s)
+    if [[ "$timenow" > "$threshhold" ]]; then
+        echo "Timeout - $timeout seconds elapsed"
+        exit 1
+    fi
+
+    # See what's deployed
+    current_version=`aws elasticbeanstalk describe-environments --application-name "$NAME-application" --environment-name "$NAME" --query "Environments[*].VersionLabel" --output text`
+
+    status=`aws elasticbeanstalk describe-environments --application-name "$NAME-application" --environment-name "$NAME" --query "Environments[*].Status" --output text`
+
+    if [ "$current_version" != "$VERSION" ]; then
+        echo "Tag not updated (currently $version). Waiting."
+        sleep 10
+        continue
+    fi
+    if [ "$status" != "Ready" ]; then
+        echo "System not Ready -it's $status. Waiting."
+        sleep 10
+        continue
+    fi
+    break
+done
